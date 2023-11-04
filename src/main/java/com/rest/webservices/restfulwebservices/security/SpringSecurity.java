e38f080f9aec7927605e983dffd277ef2f321694
Pardeshi <Shubham.Pardeshi@fujitsu.com>
2023-11-04 22:53:45 +0530

    docker installation

---
+++ src/main/java/com/rest/webservices/restfulwebservices/security/SpringSecurity.java
@@ -0,0 +1,28 @@
+package com.rest.webservices.restfulwebservices.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurity {
+	
+	
+	@Bean 
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(
+				auth ->auth.anyRequest().authenticated()
+				);
+		
+		http.httpBasic(withDefaults()); //shows web page to login
+		
+		http.csrf().disable();
+		
+		return http.build();
+	}
+	
+}
